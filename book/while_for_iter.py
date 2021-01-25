477df01fd746a1df6ecd35a20341e698c71b92c7
Dima Fedorov <dmitriy.fedoroff@gmail.com>
2021-01-25 18:34:09 +0300

    update book

---
+++ book/while_for_iter.py
@@ -0,0 +1,12 @@
+# while_for_iter.py
+x = [1, 2, 3]
+# создаем итератор:
+iterator = iter(x)
+# уходим в бесконечный цикл:
+while True:
+    try:
+        item = next(iterator) # перемещаемся по элементам
+    except StopIteration: # итератор завершился
+        del iterator
+        break
+    print(item)
